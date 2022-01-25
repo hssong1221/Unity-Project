f084e92073d80e3e7c94bd0df442f68120305966
박준영 <ib0712@naver.com>
2022-01-25 18:11:17 +0900

    간단한 로딩화면 추가 및 파일 정리

---
+++ McCree/Assets/Scenes/PunCallbacks.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace com.ThreeCS.McCree
+{
+    public class PunCallbacks : MonoBehaviourPunCallbacks
+    {
+        public static GameObject statusUI;  // 네트워크 상태 UI
+        public static Text statusText;      // 네트워크 상태 텍스트
+        // stataic 변수는 처음에 지정을 못해주는듯
+        // Awake함수에서 하위요소를 불러와서 각각 저장
+
+        private Transform[] canvasChildrens;
+
+        private void Awake()
+        {
+            canvasChildrens = transform.GetComponentsInChildren<Transform>();
+            foreach (Transform child in canvasChildrens)
+            {
+                if (child.name == "BackGround")
+                {
+                    statusUI = child.gameObject;
+                    statusUI.SetActive(false);
+                }
+                else if (child.name == "StatusText")
+                    statusText = child.gameObject.GetComponent<Text>();
+            }
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        // 마스터 서버와 연결 시 작동
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("서버 연결 성공");
+            statusText.text = "서버에 연결 성공!";
+            statusUI.SetActive(false);
+            PhotonNetwork.JoinLobby();
+        }
+
+        // 서버 접속 실패시 자동 실행
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Connecting failed!! Trying re-connect..");
+            statusText.text = "서버와 연결 실패, 접속 재시도 중...";
+            statusUI.SetActive(true);
+            // 설정한 정보로 마스터 서버 접속 시도
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            base.OnJoinedLobby();
+            Debug.Log("로비 참가 성공");
+            statusText.text = "로비 접속 성공";
+            statusUI.SetActive(false);
+            SceneManager.LoadScene("Lobby");
+        }
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            Debug.Log("방 참가 성공");
+            statusText.text = "방 참가 성공";
+            statusUI.SetActive(false);
+            SceneManager.LoadScene("Room");
+        }
+
+        public override void OnCreatedRoom()
+        {
+            base.OnCreatedRoom();
+            Debug.Log("방 만들기 성공");
+            statusText.text = "방 생성 성공";
+            statusUI.SetActive(false);
+            //SceneManager.LoadScene("Room");
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            base.OnCreateRoomFailed(returnCode, message);
+            Debug.Log("방 만들기 실패");
+        }
+    }
+
+}
