43b01785ed83b69f0ae602850556083f115d61b3
hssong1221 <hssong1221@naver.com>
2022-03-18 22:09:20 +0900

    브금 데모

--- McCree/Assets/Scenes/0. Launcher/Launcher.cs
+++ McCree/Assets/Scenes/0. Launcher/Launcher.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Launcher : MonoBehaviour
 {
+    public GameObject BackgroundMusic;
+    private AudioSource bgm;
+
+    void Awake()
+    {
+        bgm = BackgroundMusic.GetComponent<AudioSource>();
+
+        bgm.Play();
+        DontDestroyOnLoad(BackgroundMusic);
+    }
     void Start()
     {
         SceneManager.LoadScene("Login");
